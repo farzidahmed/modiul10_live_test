5f208b637e2f4bf3fcd5588858e79a72572593eb
hp <rimonraj670@gmail.com>
2024-08-14 23:10:43 +0600

    Message Here

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Modiul10());
+}
+class Modiul10 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) =>
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+}
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) =>
+      Scaffold(
+
+      );
+
+}
